87e4cf282e2ebb5354cb4bd4e1f463d39fc5a3e5
Andrew Drane <andrew@andrewdrane.co>
2011-09-25 13:45:46 -0400

    bookmarks and all on the view

--- questions/controllers/tests_controller.php
+++ questions/controllers/tests_controller.php
@@ -80,6 +80,38 @@ class TestsController extends AppController {
 			$this->redirect(array('action'=>'index'));
 		}
 	}
+    
+    /** Gests a user's bookmarked content
+     *
+     * @param type $user_id 
+     */
+    function bookmarked_questions( $test_id = null, $user_id = null ){
+        if( !$user_id ) {
+            $user_id = $this->Auth->user('id');
+        }
+        $user = $this->Test->User->find('first', array(
+            'conditions' => array('User.id' => $user_id ),
+            'contain' => array('BookmarkedQuestions'),
+        ));
+        $this->layout = false;
+        $question_options = array();
+        foreach ($user['BookmarkedQuestions'] as $question) {
+            $question_options[] = array(
+                'Question' => $question,
+            );
+        }
+        $this->set('test_id', $test_id );
+        $this->set( compact( 'question_options' ) );
+        return $this->render('/elements/tests/question_list');
+    }
+    
+    function all_questions( $test_id = null ){
+        $this->layout = false;
+        $this->Test->Question->recursive = -1;
+        $questions = $this->Test->Question->find('all');
+        $this->set('test_id', $test_id );
+        $this->set( 'question_options', $questions );
+        return $this->render('/elements/tests/question_list');
+    }
 
 }
-?>
